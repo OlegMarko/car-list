0ff09ee3f3d6362067177dc7a95998c8bd4ceb82
Oleg Marko <olegmarko7@gmail.com>
2018-08-22 08:50:45 +0300

    Added car description

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const car = (name, model, owner, year, phone, images) => ({ name, model, owner, year, phone, images });
+const car = (name, model, owner, year, phone, image) => ({ name, model, owner, year, phone, image });
 
 const cars = [
     car('Ford', 'Focus', 'Oleg', 2016, '+380 98 123 45 67', 'images/focus.jpg'),
@@ -11,7 +11,24 @@ new Vue({
     el: '#app',
     data() {
         return {
-            cars
+            cars,
+            car: cars[0],
+            selectedCar: 0,
+            phoneVisibility: false
+        }
+    },
+    methods: {
+        selectCar(index) {
+            this.car = cars[index];
+            this.selectedCar = index;
+        },
+        togglePhone() {
+            this.phoneVisibility = !this.phoneVisibility
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide Phone' : 'Show Phone';
         }
     }
 });
